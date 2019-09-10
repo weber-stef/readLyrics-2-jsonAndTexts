fd56ae35e4d41c684807e9cc71fcd73d3560a009
Leandro <leandro.frigerio@devugees.org>
2019-09-10 15:52:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const directory = './lyrics/';
+let currentType = ''
+/* Search for all the file in a given directory */
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    console.log(file);
+    readFile(file)
+  });
+});
+
+
+/* Read a single file and create the json */
+const readFile = (file) => {
+
+  fs.readFile(directory + file, 'utf8', function (err, data) {
+    if (err) throw err;
+    const arrayOfContent = data.split("\n").map(ele => {
+      if (ele.includes('Refrain')) currentType = 'Refrain'
+      if (ele.includes('Bridge')) currentType = 'Bridge'
+      if (ele.includes('Author')) currentType = 'Author'
+      if (ele === '') currentType = ''
+      return {
+        text: ele, length: ele.length, type: isUpperCase(ele) ? 'Title' : isNumber(ele) ? 'Date' : currentType
+      }
+    }).filter(line => line.text.length > 0).filter(line => line.text === 'Bridge:' || line.text === 'Refrain:' || line.text === 'Author:' || line.text === 'Date:' ? false : true)
+
+    //console.log(arrayOfContent)
+    storeData(arrayOfContent, './lyrics.json')
+  });
+
+
+  /* Append the data to the json file */
+  const storeData = (data, path) => {
+    try {
+      fs.appendFile(path, JSON.stringify(data), () => console.log('Done'));
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+
+  isUpperCase = (ele) => {
+    return /\B[A-Z]\B/g.test(ele);
+  }
+
+  isNumber = (ele) => {
+    return /^\d+$/.test(ele)
+  }
+
+
+
+
+}
+
