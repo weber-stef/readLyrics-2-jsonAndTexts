36f0d9ff6efbeec09db71cd458955bf187be5d80
Stefan Wruck <vitatech@gmx.de>
2019-09-17 15:10:27 +0200

    json file exists of multiple arrays

--- txtsplit2singletexts.js
+++ txtsplit2singletexts.js
@@ -16,19 +16,66 @@ const readFile = file => {
     if (err) throw err;
     // Split big text into single texts
     const unfilteredTexts = data.split("sw\n");
-    console.log(unfilteredTexts[200]);
-    // Split single texts into json-Arrays
-    unfilteredTexts.map(
-      song =>{
-        const singleSongArray = song.split("\n").map((singleTextLine, index) =>  {     if (singleTextLine.includes("Refrain")) {
-          console.log(singleTextLine);
-          return {
-            text: singleTextLine, type:"Refrain"
-          }
-        }
-      
-      
 
-    )}
+    // console.log(Array.isArray(unfilteredTexts));
+    unfilteredTexts.map((singleText, index) =>
+      // create single textfiles here
+
+      storeData(unfilteredTextArray(singleText), "./lyrics.json", index)
+    );
   });
+};
+///
+// function to map through single texts applying conditions
+
+const unfilteredTextArray = singleText =>
+  singleText.split("\n").map((singleTextLine, index) => {
+    if (singleTextLine.includes("Refrain")) currentType = "Refrain";
+    if (singleTextLine.includes("Bridge")) currentType = "Bridge";
+    if (singleTextLine.includes("Author")) currentType = "Author";
+    if (singleTextLine === "") currentType = "";
+    if (singleTextLine === "sw") currentType = "pg";
+    return {
+      nr: index,
+      text: singleTextLine,
+      length: singleTextLine.length,
+      type: isUpperCase(singleTextLine)
+        ? "Title"
+        : isDate(singleTextLine)
+        ? "Date"
+        : isYear(singleTextLine)
+        ? "Year"
+        : currentType
+    };
+  });
+/* Append the data to the json file */
+const storeData = (data, path, index) => {
+  try {
+    fs.appendFile(path, JSON.stringify(data), () =>
+      console.log(index + " stored")
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+isUpperCase = singleTextLine => {
+  // check for more than 2 Capital Letters in a row > The title
+  // console.log(/\b[A-Z]{2,}/g.test(singleTextLine));
+  return /\b[A-Z]{2,}/g.test(singleTextLine);
+};
+
+isDate = singleTextLine => {
+  // check for sth like (08.09)
+  return /\([0-9]{1,}\.[0-9]{1,}.{0,}\)/g.test(singleTextLine);
+};
+
+isYear = singleTextLine => {
+  // check for sth like (08.09)
+  return /[0-9]{4}./g.test(singleTextLine);
+};
+
+isAuthor = singleTextLine => {};
+
+isNumber = singleTextLine => {
+  return /^\d+$/.test(singleTextLine);
 };
